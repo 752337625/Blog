b6630ed8ef7290cbeaa214eaad5dd43da0879af9
wanghongwei <wanghongwei1@bonc.com.cn>
2020-06-12 18:10:16 +0800

    添加swiper组件库

--- blogapp-web/src/components/Section-swiper.vue
+++ blogapp-web/src/components/Section-swiper.vue
@@ -1,17 +1,18 @@
 <template>
 	<div class="page-swiper">
 		<template>
-			<swiper ref="mySwiper" :options="swiperOptions">
-				<swiper-slide>Slide 1</swiper-slide>
-				<swiper-slide>Slide 2</swiper-slide>
-				<swiper-slide>Slide 3</swiper-slide>
-				<swiper-slide>Slide 4</swiper-slide>
-				<swiper-slide>Slide 5</swiper-slide>
-				<swiper-slide>Slide 6</swiper-slide>
-				<swiper-slide>Slide 7</swiper-slide>
-				<swiper-slide>Slide 8</swiper-slide>
-				<swiper-slide>Slide 9</swiper-slide>
-				<swiper-slide>Slide 10</swiper-slide>
+			<swiper :options="swiperOption" ref="mySwiper" >
+				<swiper-slide>I'm Slide 1</swiper-slide>
+				<swiper-slide>I'm Slide 2</swiper-slide>
+				<swiper-slide>I'm Slide 3</swiper-slide>
+				<swiper-slide>I'm Slide 4</swiper-slide>
+				<swiper-slide>I'm Slide 5</swiper-slide>
+				<swiper-slide>I'm Slide 6</swiper-slide>
+				<swiper-slide>I'm Slide 7</swiper-slide>
+				<div class="swiper-pagination" slot="pagination"></div>
+				<div class="swiper-button-prev" slot="button-prev"></div>
+				<div class="swiper-button-next" slot="button-next"></div>
+				<div class="swiper-scrollbar" slot="scrollbar"></div>
 			</swiper>
 		</template>
 	</div>
@@ -29,14 +30,24 @@
 		},
 		data( ) {
 			return {
-				swiperOptions: {
-					slideClass: 'my-slide'
-				},
+				swiperOption: {
+					// some swiper options/callbacks
+					// 所有的参数同 swiper 官方 api 参数
+					// ...
+				}
 			}
 		},
-		mounted( ) {},
-		methods: {},
-		computed: {},
+		computed: {
+			swiper( ) {
+				return this.$refs.mySwiper.swiper
+			}
+		},
+		mounted( ) {
+			// current swiper instance
+			// 然后你就可以使用当前上下文内的swiper对象去做你想做的事了
+			console.log( 'this is current swiper instance object', this.swiper )
+			this.swiper.slideTo( 3, 1000, false )
+		}
 	}
 </script>
 <style lang="less">
